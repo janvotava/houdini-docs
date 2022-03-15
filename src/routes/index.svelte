5ff6397f3b0d850102d88ff7b5ba3cea19356665
Alec Aivazis <alec@aivazis.com>
2022-03-15 01:42:04 -0700

    link directly to page from landing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -96,8 +96,8 @@
 		<img src="/images/logo.svg" alt="Houdini Logo" width="175px" />
 	</a>
 	<nav>
-		<a href="/intro" class="nav-link" sveltekit:prefetch>Get Started</a>
-		<a href="/api" class="nav-link micro-hidden" sveltekit:prefetch>API</a>
+		<a href="/intro/welcome" class="nav-link" sveltekit:prefetch>Get Started</a>
+		<a href="/api/" class="nav-link micro-hidden" sveltekit:prefetch>API</a>
 		<a href="https://opencollective.com/houdini" class="nav-link small-hidden" target="_blank">
 			Support
 		</a>
